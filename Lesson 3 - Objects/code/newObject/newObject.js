95efe24c3bdb401273eeb539c206db6846a67d3c
Konrad Perzyna <konrad@perzyna.net>
2016-03-01 23:11:46 +0100

    newObject

--- Lesson 3 - Objects/code/newObject/newObject.js
+++ Lesson 3 - Objects/code/newObject/newObject.js
@@ -3,16 +3,32 @@
 		global.UAM = {};
 	}
 
-	function newObject() {}
+	function newObject(constructor) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var obj = Object.create(constructor.prototype); 
+        
+        // TODO: Dlaczego to daje dubluje __proto__ Object{__proto__: Person, __proto__ : Person} ?
+        // var obj = Object.create(null);
+        // obj.__proto__ = constructor.prototype;  
+        // console.log(obj);
+
+        constr_obj = constructor.apply(obj, args);
+        if (constr_obj) {
+            obj = constr_obj;
+        }
+        return obj;
+    }
 
 	global.UAM.newObject = newObject;
 }(window));
 
 /*
-	Zaimplementuj funkcję newObject, która będzie działać analogicznie do operatora new. Pierwszym parametrem funkcji niech będzie
+	Zaimplementuj funkcję newObject, która będzie działać analogicznie do operatora new. 
+    Pierwszym parametrem funkcji niech będzie
 	konstruktor, natomiast pozostałe to parametry konstruktora. Przykładowe zastosowanie:
 
 	new MyClass(arg1, arg2) -> newObject(MyClass, arg1, arg2)
 */
 
+//call lub apply - tablica jako lista  parametrów
 

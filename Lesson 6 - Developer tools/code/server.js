f48433db1eb55e8b60003ee1bbb52fd13e2a1f66
Konrad Perzyna <konrad@perzyna.net>
2016-03-22 21:03:58 +0100

    Zadanie 6.1. Kalkulator

--- Lesson 6 - Developer tools/code/server.js
+++ Lesson 6 - Developer tools/code/server.js
@@ -16,11 +16,48 @@ app.get('/today', function (req, res) {
 });
 
 //POST /numbers should save number for later
+app.post('/numbers', function (req, res) {
+  numbers.push(+req.body);
+  res.send('Number stored');
+});
+
 //GET /numbers should return comma separated list of numbers
+app.get('/numbers', function (req, res) {
+    setTimeout(function () {
+        res.send(numbers.join(", "));
+    }, 1000);
+});
+
 //DELETE /numbers should empty numbers array
+app.delete('/numbers', function (req, res) {
+  numbers = [];
+  res.send('Numbers deleted');
+});
+
 //GET /numbers/:op should return result of given operation of stored numbers (check request.params)
 // available operations are sum, difference, multiplication, division
 // return result as strings
+app.get('/numbers/:op', function (req, res) {
+    setTimeout(function () {
+        switch(req.params.op) {
+            case 'sum' : 
+                res.send(numbers.reduce((a, b) => a + b, 0).toString());
+                break;
+            case 'difference' : 
+                res.send(numbers.slice(1).reduce((a, b) => a - b, numbers[0]).toString());
+                break;
+            case 'multiplication' : 
+                res.send(numbers.reduce((a, b) => a * b, 1).toString());
+                break;
+            case 'division' : 
+                res.send(numbers.slice(1).reduce((a, b) => a / b, numbers[0]).toString());
+                break;      
+            default : res.send('Unsupported operation');                          
+        }
+        
+    }, 1000);
+});
+
 
 app.listen(8080, function () {
     console.log('Server is running on localhost:' + this.address().port);

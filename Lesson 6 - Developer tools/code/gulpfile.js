2d0bbf6ee49e676707efa2cb2df435568a911d00
Konrad Perzyna <konrad@perzyna.net>
2016-03-23 21:55:33 +0100

    Dodane dependencies w gulpfile.js. Dodany plik konfiguracyjny dla jshint. Dodany plik .gitignore.

--- Lesson 6 - Developer tools/code/gulpfile.js
+++ Lesson 6 - Developer tools/code/gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
+/*jshint node:true */
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint');
 
 gulp.task('jshint', function() {
     gulp.src(['*', 'src/*.js'])
-        .pipe(jshint())
+        .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
-        //.pipe(jshint.reporter('fail'))
+        //.pipe(jshint.reporter(.jshintrc'fail'))
         ;
 });
 

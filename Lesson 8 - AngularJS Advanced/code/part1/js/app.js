2986aadd4de254fefa174e7205ef80e21eb3f2fe
Konrad Perzyna <konrad@perzyna.net>
2016-04-19 17:10:15 +0200

    Lesson 8. From classes.

--- Lesson 8 - AngularJS Advanced/code/part1/js/app.js
+++ Lesson 8 - AngularJS Advanced/code/part1/js/app.js
@@ -1,3 +1,8 @@
 angular.module('myapp', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
-
+    $stateProvider
+    .state('users', {
+        url: "/users",
+        controller: 'UsersCtrl',
+        templateUrl: 'partials/users.html'   
+    });
 });

f63a373f2f50daaa349dda2c6c1fe4ddc4922dc5
Konrad Perzyna <konrad@perzyna.net>
2016-04-26 16:58:38 +0200

    Lesson 9

--- Lesson 9 - Promises and Ajax/code/http/js/app.js
+++ Lesson 9 - Promises and Ajax/code/http/js/app.js
@@ -2,6 +2,20 @@ angular.module('app', []).controller('AppCtrl', function ($scope, $http) {
 	$scope.results = [];
 
 	$scope.clickElement = function (element) {
-
+        element.count += 1;
+        $http.post('./elements/'+element.id, element);
 	};
+
+    
+   $scope.loadUsers = function (){
+        $http.get('./elements')
+        .success(function (data, status, headers, config, statusText) {
+            $scope.results = data;
+        })
+        .error(function (data, status, headers, config, statusText) {
+
+        });
+    };
 });
+
+
